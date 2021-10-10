48001229c3a32ba1a229ff7961959cb92d491e11
NJ00 <nj00@outlook.com>
2021-10-10 19:14:19 +0900

    NextComの動作環境に変更

--- CognitiveServicesDemo/dev_Settings.cs
+++ CognitiveServicesDemo/dev_Settings.cs
@@ -5,22 +5,22 @@
         // ConnectionStrings of Microsoft SQL database (loac or Azure) is in "appsetting.json".
 
         // Computer Vision API (Cognitive Services) key and endpoint
-        public const string computervision_subscriptionKey = "56351b0970694b51be73761704752e78";
-        public const string computervision_endpoint = "https://nativesense-test.cognitiveservices.azure.com/";
+        public const string computervision_subscriptionKey = "17556dc1c6e046ad8a5a3b187056aeee";
+        public const string computervision_endpoint = "https://pinteresttest.cognitiveservices.azure.com/";
         public const string computervision_language = "ja";
 
         // Cognitive Search API (Cognitive Services) admin key and indexs
-        public const string cognitivesearch_adminApiKey = "6AD872ADFC6E014336424204D698127F";
-        public const string cognitivesearch_endpoint = "https://cspa-cognitivesearch.search.windows.net";
+        public const string cognitivesearch_adminApiKey = "66369D72061A0D03FF963060DFE15F07";
+        public const string cognitivesearch_endpoint = "https://nxsearchtest.search.windows.net";
         public const string cognitivesearch_index_SQL = "azuresql-usermedia-index";
         public const string cognitivesearch_index_Cosmos = "azurecosmos-usermedia-index";
         public const string cognitivesearch_index_Table = "azuretable-usermedia-index";
         public const string cognitivesearch_index_Blob = "azureblob-usermedia-index";
 
         // Azure Storage account
-        public const string storage_accountName = "cspastorage1";
-        public const string storage_accountKey = "V33ZcXYoEziBBuhFB9CyKtyzQSCPqR0jLbCct22MrisesNOoYOvEBownppWLrw/bNvWUjcqpsKX7p4b+Bio3Mg==";
-        public const string storage_connectionString = "DefaultEndpointsProtocol=https;AccountName=cspastorage1;AccountKey=V33ZcXYoEziBBuhFB9CyKtyzQSCPqR0jLbCct22MrisesNOoYOvEBownppWLrw/bNvWUjcqpsKX7p4b+Bio3Mg==;EndpointSuffix=core.windows.net";
+        public const string storage_accountName = "pinteresttest";
+        public const string storage_accountKey = "dnQo3qvSaASRb29UQKsn9yGvzyD+J6lj1B4joQVRWIJRe8h2upude5ZoeWmo2moJHvUYayGaUM3bxMsGKnktfg==";
+        public const string storage_connectionString = "DefaultEndpointsProtocol=https;AccountName=pinteresttest;AccountKey=dnQo3qvSaASRb29UQKsn9yGvzyD+J6lj1B4joQVRWIJRe8h2upude5ZoeWmo2moJHvUYayGaUM3bxMsGKnktfg==;EndpointSuffix=core.windows.net";
 
         // Azure Storage Blob container for storing images.
         public const string blob_containerName_image = "imagecontainer";
@@ -37,8 +37,8 @@
         //    Not need to configure it here (configure in appsetting.json).
 
         // 2) Azure Cosmos DB
-        public const string cosmos_endpointUri = "https://cspa-cosmos.documents.azure.com:443/";
-        public const string cosmos_accountKey = "Sj73OxoJPoJjLzOjsMOZ4JMw8AR1Oo1C3A27vkErL0AibqOP006Wxg1IdU2VMoNzObpBQEAHKc5fxWaCsRPYRw==";
+        public const string cosmos_endpointUri = "https://pinteresttest.documents.azure.com:443/";
+        public const string cosmos_accountKey = "OEF35KY8D7F3Rjda1EOXSchc3h15d7wkxhN43tER0mGRTlbptXAN7eJ7edss5bvy39QbgWa7rHbwE8N8E695JA==";
         public const string cosmos_databaseName = "cspacosmos";
         public const string cosmos_containerName = "UserMedia";
 
